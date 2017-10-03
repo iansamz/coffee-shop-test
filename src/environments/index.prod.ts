72f9f02dca33c407a223ac438dd491da59da7511
Ian Sam Mungai <iansam74@gmail.com>
2017-10-03 23:19:11 +0300

    Update index.prod.ts

--- src/environments/index.prod.ts
+++ src/environments/index.prod.ts
@@ -1,13 +1,16 @@
 // This file contains production variables. (When you work in PROD MODE)
 // This file is use by webpack. Please don't rename it and don't move it to another directory.
+
+// get from firebase
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAEy_H3WZj8R9nE1p3tjtF-oWNKkPmZDLQ",
-    authDomain: "gardenretreatcentre.firebaseapp.com",
-    databaseURL: "https://gardenretreatcentre.firebaseio.com",
-    projectId: "gardenretreatcentre",
-    storageBucket: "gardenretreatcentre.appspot.com",
-    messagingSenderId: "72484553724"
+    apiKey: "yours",
+    authDomain: "yours",
+    databaseURL: "yours",
+    projectId: "yours",
+    storageBucket: "yours",
+    messagingSenderId: "yours"
   }
 };
+
